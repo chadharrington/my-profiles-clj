7852917d5810ea8f32df3c8fc2d1882c6a9d4664
Chad Harrington <chad@deercreeklabs.com>
2014-09-23 14:46:16 -0600

    Add initial files

---
+++ profiles.clj
@@ -0,0 +1,7 @@
+{:user 
+ {:signing {:gpg-key "chad@farbetter.com"}
+  :plugins [[cider/cider-nrepl "0.7.0"]
+            [lein-ancient "0.5.5"]
+            [lein-cloverage "1.0.2"]
+            [lein-midje "3.1.3"]]}}
+
