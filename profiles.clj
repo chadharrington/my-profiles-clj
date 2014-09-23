abd3239769a2d09c18fb118ff5e5a6dd1f158f65
Chad Harrington <chad@deercreeklabs.com>
2014-09-23 15:25:31 -0600

    Add lein-autoexpect

--- profiles.clj
+++ profiles.clj
@@ -2,6 +2,7 @@
  {:signing {:gpg-key "chad@farbetter.com"}
   :plugins [[cider/cider-nrepl "0.7.0"]
             [lein-ancient "0.5.5"]
+            [lein-autoexpect "1.2.2"]
             [lein-cloverage "1.0.2"]
             [lein-midje "3.1.3"]]}}
 
