cbc4d9ab6624da392a9399c1005a7663c317f18b
Chad Harrington <chad@farbetter.com>
2014-10-13 20:13:34 -0600

    Remove unused plugins. Add cloverage and lein-test-refresh.

--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,9 @@
 {:user 
  {:signing {:gpg-key "chad@farbetter.com"}
+  :dependencies [[pjstadig/humane-test-output "0.6.0"]]
+  :injections [(require 'pjstadig.humane-test-output)
+               (pjstadig.humane-test-output/activate!)]
   :plugins [[cider/cider-nrepl "0.7.0"]
+            [com.jakemccrary/lein-test-refresh "0.5.4"]
             [lein-ancient "0.5.5"]
-            [lein-autoexpect "1.2.2"]
-            [lein-cloverage "1.0.2"]
-            [lein-midje "3.1.3"]]}}
-
+            [lein-cloverage "1.0.2"]]}}
