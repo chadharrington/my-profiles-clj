2e65701a6f6b70123e2f7e619977977192d54802
Chad Harrington <chad@farbetter.com>
2014-12-20 13:27:50 -0700

    Update cider-nrepl. Add lein-noderepl.

--- profiles.clj
+++ profiles.clj
@@ -3,7 +3,8 @@
   :dependencies [[pjstadig/humane-test-output "0.6.0"]]
   :injections [(require 'pjstadig.humane-test-output)
                (pjstadig.humane-test-output/activate!)]
-  :plugins [[cider/cider-nrepl "0.7.0"]
+  :plugins [[cider/cider-nrepl "0.8.1"]
             [com.jakemccrary/lein-test-refresh "0.5.4"]
             [lein-ancient "0.5.5"]
-            [lein-cloverage "1.0.2"]]}}
+            [lein-cloverage "1.0.2"]
+            [org.bodil/lein-noderepl "0.1.11"]]}}
