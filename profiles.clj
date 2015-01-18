dc2b92484b3d324e35d3b59de8d88bb88164e2fc
Chad Harrington <chad@farbetter.com>
2015-01-18 01:06:04 -0700

    Add eastwood, kibit. Update cider-nrepl and lein-test-refresh. Remove bodil's noderepl.

--- profiles.clj
+++ profiles.clj
@@ -1,10 +1,13 @@
-{:user 
+{:user
  {:signing {:gpg-key "chad@farbetter.com"}
   :dependencies [[pjstadig/humane-test-output "0.6.0"]]
   :injections [(require 'pjstadig.humane-test-output)
                (pjstadig.humane-test-output/activate!)]
-  :plugins [[cider/cider-nrepl "0.8.1"]
-            [com.jakemccrary/lein-test-refresh "0.5.4"]
+  :plugins [[cider/cider-nrepl "0.8.2"]
+            [com.jakemccrary/lein-test-refresh "0.5.5"]
+            [jonase/eastwood "0.2.1"]
             [lein-ancient "0.5.5"]
             [lein-cloverage "1.0.2"]
-            [org.bodil/lein-noderepl "0.1.11"]]}}
+            [lein-kibit "0.0.8"]]
+  :test-refresh {:notify-command ["terminal-notifier"
+                                  "-title" "Tests" "-message"]}}}
