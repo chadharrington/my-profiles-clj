502dd745b4016c73cf75a6693247a690e45c1314
Chad Harrington <chad@farbetter.com>
2015-02-12 13:50:41 -0700

    Add lein-bikeshed

--- profiles.clj
+++ profiles.clj
@@ -7,6 +7,7 @@
             [com.jakemccrary/lein-test-refresh "0.5.5"]
             [jonase/eastwood "0.2.1"]
             [lein-ancient "0.5.5"]
+            [lein-bikeshed "0.2.0"]
             [lein-cloverage "1.0.2"]
             [lein-kibit "0.0.8"]]
   :test-refresh {:notify-command ["terminal-notifier"
