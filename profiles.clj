0af1c9efdbc3b631866d7c1443a87d9c4f1267a4
Chad Harrington <chad@farbetter.com>
2015-03-15 02:55:05 -0600

    Add lein-try

--- profiles.clj
+++ profiles.clj
@@ -10,6 +10,7 @@
             [lein-bikeshed "0.2.0"]
             [lein-cloverage "1.0.2"]
             [lein-cprint "1.0.0"]
-            [lein-kibit "0.0.8"]]
+            [lein-kibit "0.0.8"]
+            [lein-try "0.4.3"]]
   :test-refresh {:notify-command ["terminal-notifier"
                                   "-title" "Tests" "-message"]}}}
