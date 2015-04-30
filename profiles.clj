358a9b5db95b80936452f7e26d7325605ecdfbaa
Chad Harrington <chad@farbetter.com>
2015-04-30 12:52:35 -0600

    Update versions

--- profiles.clj
+++ profiles.clj
@@ -4,13 +4,13 @@
   :injections [(require 'pjstadig.humane-test-output)
                (pjstadig.humane-test-output/activate!)]
   :plugins [[cider/cider-nrepl "0.8.2"]
-            [com.jakemccrary/lein-test-refresh "0.7.0"]
+            [com.jakemccrary/lein-test-refresh "0.9.0"]
             [jonase/eastwood "0.2.1"]
-            [lein-ancient "0.6.3"]
+            [lein-ancient "0.6.7"]
             [lein-bikeshed "0.2.0"]
-            [lein-cloverage "1.0.2"]
+            [lein-cloverage "1.0.3"]
             [lein-cprint "1.0.0"]
-            [lein-kibit "0.0.8"]
+            [lein-kibit "0.1.2"]
             [lein-try "0.4.3"]]
   :test-refresh {:notify-command ["terminal-notifier"
                                   "-title" "Tests" "-message"]}}}
