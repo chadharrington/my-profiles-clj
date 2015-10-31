924eb508c39ace5e173912c7b6d4cf70d5ec9327
Chad Harrington <chad@farbetter.com>
2015-10-31 15:18:42 -0600

    Update plugin versions

--- profiles.clj
+++ profiles.clj
@@ -14,15 +14,14 @@
 
   :plugins
   [[cider/cider-nrepl "0.9.1"]
-   [com.jakemccrary/lein-test-refresh "0.10.0"]
+   [com.jakemccrary/lein-test-refresh "0.11.0"]
    [jonase/eastwood "0.2.1"]
    [lein-ancient "0.6.7"]
    [lein-bikeshed "0.2.0"]
-   [lein-cljfmt "0.2.0"]
+   [lein-cljfmt "0.3.0"]
    [lein-cloverage "1.0.6"]
    [lein-cprint "1.0.0"]
    [lein-kibit "0.1.2"]
-   [lein-localrepo "0.5.3"]
    [lein-try "0.4.3"]
    [lein-vanity "0.2.0"]]
 
