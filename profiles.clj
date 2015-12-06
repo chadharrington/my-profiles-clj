f729e0ae70f488e40191a5548ee2d2473f0969a7
Chad Harrington <chad@farbetter.com>
2015-12-05 20:03:14 -0700

    remove puget dep.

--- profiles.clj
+++ profiles.clj
@@ -2,15 +2,14 @@
  {:signing {:gpg-key "chad@farbetter.com"}
   :dependencies
   [[com.cemerick/piggieback "0.2.1"]
-   [mvxcvi/puget "0.8.1"]
    [org.clojure/tools.nrepl "0.2.11"]
    [pjstadig/humane-test-output "0.7.0"]]
 
-  :repl-options
-  {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
-
   :injections [(require 'pjstadig.humane-test-output)
                (pjstadig.humane-test-output/activate!)]
+
+  :repl-options
+  {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
   :plugins
   [[cider/cider-nrepl "0.9.1"]
