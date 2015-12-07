047b224a11d346f046cc46220990eac315df9117
Chad Harrington <chad@farbetter.com>
2015-12-07 14:46:53 -0700

    Update versions.

--- profiles.clj
+++ profiles.clj
@@ -2,8 +2,8 @@
  {:signing {:gpg-key "chad@farbetter.com"}
   :dependencies
   [[com.cemerick/piggieback "0.2.1"]
-   [org.clojure/tools.nrepl "0.2.11"]
-   [pjstadig/humane-test-output "0.7.0"]]
+   [org.clojure/tools.nrepl "0.2.12"]
+   [pjstadig/humane-test-output "0.7.1"]]
 
   :injections [(require 'pjstadig.humane-test-output)
                (pjstadig.humane-test-output/activate!)]
@@ -12,14 +12,14 @@
   {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
   :plugins
-  [[cider/cider-nrepl "0.9.1"]
+  [[cider/cider-nrepl "0.10.0"]
    [com.jakemccrary/lein-test-refresh "0.11.0"]
-   [jonase/eastwood "0.2.1"]
-   [lein-ancient "0.6.7"]
+   [jonase/eastwood "0.2.2"]
+   [lein-ancient "0.6.8"]
    [lein-bikeshed "0.2.0"]
    [lein-cljfmt "0.3.0"]
    [lein-cloverage "1.0.6"]
-   [lein-cprint "1.0.0"]
+   [lein-cprint "1.2.0"]
    [lein-kibit "0.1.2"]
    [lein-try "0.4.3"]
    [lein-vanity "0.2.0"]]
