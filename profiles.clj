8190a95e5e842620a6e40e9fd5c3c941fc2ab1b6
Chad Harrington <chad@farbetter.com>
2016-05-25 18:56:20 -0600

    Add :dev profile and lein-cloverage.

--- profiles.clj
+++ profiles.clj
@@ -1,10 +1,5 @@
 {:user
  {:signing {:gpg-key "chad@farbetter.com"}
-  :dependencies
-  [[com.cemerick/piggieback "0.2.1"]
-   [org.clojure/tools.nrepl "0.2.12"]
-   [pjstadig/humane-test-output "0.8.0"]]
-
   :injections [(require 'pjstadig.humane-test-output)
                (pjstadig.humane-test-output/activate!)]
 
@@ -16,6 +11,7 @@
    [com.jakemccrary/lein-test-refresh "0.14.0"]
    [lein-ancient "0.6.10" :exclusions [org.clojure/clojure commons-codec]]
    [lein-bikeshed "0.3.0" :exclusions [org.clojure/clojure]]
+   [lein-cloverage "1.0.6"]
    [lein-cprint "1.2.0" :exclusions [org.clojure/clojure]]
    ;; Because of confusion with a defunct project also called
    ;; lein-release, we exclude lein-release from lein-ancient.
@@ -24,5 +20,16 @@
    [lein-kibit "0.1.2" :exclusions [org.clojure/clojure]]
    [lein-try "0.4.3"]]
 
+  :dependencies
+  [[com.cemerick/piggieback "0.2.1"]
+   [org.clojure/tools.nrepl "0.2.12"]
+   [pjstadig/humane-test-output "0.8.0"]]
+
   :test-refresh {:notify-command ["terminal-notifier"
-                                  "-title" "Tests" "-message"]}}}
+                                  "-title" "Tests" "-message"]}}
+
+ :dev
+ {:source-paths ["dev"]
+
+  :dependencies
+  [[org.clojure/tools.namespace "0.2.11"]]}}
