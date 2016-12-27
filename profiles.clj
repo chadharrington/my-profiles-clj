98956f3290cefb83280df7e149fa73cbd03b9287
Chad Harrington <chad@farbetter.com>
2016-12-27 16:02:43 -0700

    Upgrade deps.

--- profiles.clj
+++ profiles.clj
@@ -7,17 +7,20 @@
   {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
   :plugins
-  [[com.jakemccrary/lein-test-refresh "0.17.0"]
+  [[com.jakemccrary/lein-test-refresh "0.18.1"]
    [lein-ancient "0.6.10"
     :exclusions [commons-codec org.apache.httpcomponents/httpclient
                  org.clojure/clojure]]
-   [lein-bikeshed "0.3.0" :exclusions [org.clojure/clojure]]
-   [lein-cljfmt "0.5.5"]
-   [lein-cloverage "1.0.6"]
+   [lein-bikeshed "0.4.1"
+    :exclusions [org.clojure/clojure org.clojure/tools.cli]]
+   [lein-cljfmt "0.5.6" :exclusions [org.clojure/clojure]]
+   [lein-cloverage "1.0.9"]
    [lein-cprint "1.2.0" :exclusions [org.clojure/clojure]]
    [lein-pprint "1.1.2"]
    [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
-   [lein-kibit "0.1.2" :exclusions [org.clojure/clojure]]
+   [lein-kibit "0.1.3" :exclusions [org.clojure/clojure]]
+   ;;   [cider/cider-nrepl "0.14.0"]
+   ;;   [cider/cider-nrepl "0.15.0-SNAPSHOT"]
    [lein-try "0.4.3"]]
 
   ;; For Charles proxying
@@ -33,8 +36,9 @@
   [[com.cemerick/piggieback "0.2.1"
     :exclusions [org.clojure/clojurescript org.hamcrest/hamcrest-core]]
    [org.clojure/tools.namespace "0.2.11"]
-   [org.clojure/tools.nrepl "0.2.12"]
    [pjstadig/humane-test-output "0.8.1"]]
 
   :test-refresh {:notify-command ["terminal-notifier"
-                                  "-title" "Tests" "-message"]}}}
+                                  "-title" "Tests" "-message"]}}
+
+ :repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
