43bbc00c32ca24c0e1df53bc03e12a53ebd6fc3a
Chad Harrington <chad@deercreeklabs.com>
2017-11-07 21:25:33 -0700

    Update pkgs.

--- profiles.clj
+++ profiles.clj
@@ -4,18 +4,19 @@
                (pjstadig.humane-test-output/activate!)]
 
   :plugins
-  [[com.jakemccrary/lein-test-refresh "0.20.0"]
-   [lein-bikeshed "0.4.1"
+  [[com.jakemccrary/lein-test-refresh "0.21.1"]
+   [lein-bikeshed "0.5.0"
     :exclusions [org.clojure/clojure org.clojure/tools.cli]]
-   [lein-cljfmt "0.5.7" :exclusions [org.clojure/clojure]]
-   [lein-cprint "1.2.0" :exclusions [org.clojure/clojure]]
-   [jonase/eastwood "0.2.4" :exclusions [org.clojure/clojure]]
-   [lein-kibit "0.1.5" :exclusions [org.clojure/clojure]]
+   [lein-cljfmt "0.5.7"
+    :exclusions [org.clojure/clojure org.clojure/tools.reader]]
+   [lein-cprint "1.3.0" :exclusions [org.clojure/clojure]]
+   [jonase/eastwood "0.2.5" :exclusions [org.clojure/clojure]]
+   [lein-kibit "0.1.6" :exclusions [org.clojure/clojure]]
    [lein-try "0.4.3"]]
 
   :dependencies
   [[org.clojure/tools.nrepl "0.2.13"]
-   [pjstadig/humane-test-output "0.8.2"]]
+   [pjstadig/humane-test-output "0.8.3"]]
 
   :test-refresh {:notify-command ["terminal-notifier"
                                   "-title" "Tests" "-message"]}}
